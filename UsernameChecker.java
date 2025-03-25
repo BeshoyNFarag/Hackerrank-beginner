455a859369d9cc670bb77399ed59d7c5d075219c
Beshoy Farag <beshoynady401@gmail.com>
2025-03-25 17:56:15 +0100

    problem solved for only one input at a time

--- UsernameChecker.java
+++ UsernameChecker.java
@@ -2,11 +2,46 @@ import java.util.Scanner;
 
 
 public class UsernameChecker {
-    public static void main(String [] args){
-        Scanner scanner =  new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         int inputSize = input.length();
+        boolean valid = true;
 
-        
+
+        for (int i = 0; i < inputSize; i++) {
+            if (inputSize < 8) {
+                valid = false;
+                break;
+            }
+            else if (Character.isDigit(input.charAt(0))) {
+
+                valid = false;
+                break;
+            }
+            else if (input.substring(i, i+1 ).matches("[A-Za-z0-9_]") != true) {
+                valid = false;
+                break;
+
+            }
+
+            else {
+                valid = true;
+
+            }
+
+
+        }
+        if(valid) System.out.println("Valid");
+
+        else System.out.println("Invalid");
+
+
+
+
+
+
+
     }
 }
+

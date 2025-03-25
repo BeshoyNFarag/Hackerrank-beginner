d90b643019cb351a41f428400d8c55353c4f750a
Beshoy Farag <beshoynady401@gmail.com>
2025-03-25 16:57:43 +0100

    create and start file

---
+++ UsernameChecker.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class UsernameChecker {
+    public static void main(String [] args){
+        Scanner scanner =  new Scanner(System.in);
+        String input = scanner.nextLine();
+        int inputSize = input.length();
+
+        
+    }
+}

507afb2bd411c62fbada19c76d6b9e2fc902392b
Beshoy Farag <beshoynady401@gmail.com>
2025-03-26 16:34:36 +0100

    finish Username checker

--- UsernameChecker.java
+++ UsernameChecker.java
@@ -4,44 +4,78 @@ import java.util.Scanner;
 public class UsernameChecker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        int inputSize = input.length();
+        int inputNumbers = scanner.nextInt();
+        scanner.nextLine();
+        String[] usernames = new String[inputNumbers];
+
+
         boolean valid = true;
 
 
-        for (int i = 0; i < inputSize; i++) {
-            if (inputSize < 8) {
-                valid = false;
-                break;
-            }
-            else if (Character.isDigit(input.charAt(0))) {
+        for (int j = 0; j < inputNumbers; j++) {
+            usernames[j] = scanner.nextLine();
 
-                valid = false;
-                break;
-            }
-            else if (input.substring(i, i+1 ).matches("[A-Za-z0-9_]") != true) {
-                valid = false;
-                break;
+        }
+        for (int x = 0; x < inputNumbers; x++) {
+            String input = usernames[x];
+            int inputSize = input.length();
+
+
+            for (int i = 0; i < inputSize; i++) {
+                if (inputSize < 8 || inputSize > 30) {
+                    valid = false;
+                    break;
+                }
+                else if (Character.isDigit(input.charAt(0)) || !input.substring(0,1).matches("[A-Za-z0-9]") ) {
+
+                    valid = false;
+                    break;
+                }
+                else if (input.substring(i, i+1 ).matches("[A-Za-z0-9_]") != true) {
+                    valid = false;
+                    break;
+
+                }
+
+                else {
+                    valid = true;
+
+                }
+
+
 
             }
+            if(valid) System.out.println("Valid");
 
-            else {
-                valid = true;
-
-            }
+            else System.out.println("Invalid");
 
 
         }
-        if(valid) System.out.println("Valid");
-
-        else System.out.println("Invalid");
-
-
-
-
-
-
-
     }
+
+
+
+
+
+
+
+
+
+
 }
+
+/*
+* input
+* 8
+Julia
+Samantha
+Samantha_21
+1Samantha
+Samantha?10_2A
+JuliaZ007
+Julia@007
+_Julia007
+*
+* */
+
 

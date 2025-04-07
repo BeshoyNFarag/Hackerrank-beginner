bd1d85c234549de6b4998700b8caa63a316d368f
Beshoy Farag <beshoynady401@gmail.com>
2025-04-07 19:09:33 +0200

    add solution for ArrayList hackerrank

---
+++ ArrayListPractice.java
@@ -0,0 +1,62 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+
+
+public class ArrayListPractice {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> numbers = new ArrayList<>();
+        int lines = scanner.nextInt(); scanner.nextLine();
+        int [] [] arrays = new int [lines] [];
+        int intInLine;
+        int queries;
+        int row, column;
+
+        for (int i = 0; i < lines; i++) {
+            intInLine = scanner.nextInt();
+            arrays[i] = new int[intInLine];
+            for (int j = 0; j < intInLine; j++) {
+                arrays[i][j] = scanner.nextInt();
+            }
+        }
+
+
+        for( int [] n  : arrays ){
+
+
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int x : n ){
+
+                list.add(x);
+            }
+            numbers.add(list);
+
+        }
+        queries = scanner.nextInt();
+        scanner.nextLine();
+        while(queries > 0){
+            row = scanner.nextInt();
+            column = scanner.nextInt();
+
+            try{
+
+                System.out.println(numbers.get(row-1).get(column-1));
+            }
+            catch(Exception e){
+                System.out.println("ERROR!");
+            }
+            queries--;
+
+
+        }
+
+
+
+
+
+
+    }
+}

f9c9f90bf36e74f9fac08ff75009e984cc7c31cf
Beshoy Farag <beshoynady401@gmail.com>
2025-04-07 19:28:43 +0200

    finish the list hackerrank challenge

--- List.java
+++ List.java
@@ -1,47 +1,62 @@
+/*
+* 5
+12 0 1 78 12
+2
+Insert
+5 23
+Delete
+0
+
+* 0 1 78 12 23
+* */
+
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class List {
+public class List{
     public static void main(String [] args){
+        ArrayList<Integer> numbers = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i = 0; i < size; i++){
-            list.add(i,scanner.nextInt());  ;
+        int size;
+        int queries;
+        int insertIndex;
+        int insertNumber;
+        int deleteIndex;
+        String command;
+        size = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0 ; i < size ; i++){
+            numbers.add(scanner.nextInt());
         }
 
-        int queries = scanner.nextInt();
+        queries = scanner.nextInt();
 
-        ArrayList<String> queriesList = new ArrayList<>();
-        String input = scanner.nextLine();
 
-        while(queries > 0 ){
-
-            if(input.equals("Insert")){
-                int x = scanner.nextInt();
-                int y = scanner.nextInt();
-                list.add(x,y);
-                queries --;
+        while(queries >0){
+            command = scanner.nextLine();
+            if(command.equals("Delete")){
+                deleteIndex = scanner.nextInt();
+                numbers.remove(deleteIndex);
+                queries--;
+            }
+            else if(command.equals("Insert")){
+                insertIndex = scanner.nextInt();
+                insertNumber = scanner.nextInt();
+                numbers.add(insertIndex, insertNumber);
+                queries--;
 
             }
-            else if(input.equals("Delete")){
-                int x = scanner.nextInt();
-                list.remove(0);
-                queries --;
-
-            }
-            input = scanner.nextLine();
-
 
 
 
         }
-
-        for(int x : list){
-            System.out.print(x+" ");
-
-
+        for(int n : numbers){
+            System.out.print(n + " ");
         }
+
+
 
     }
 }

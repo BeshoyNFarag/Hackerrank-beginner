dfcdfa06545778b4f37ac03108ebcad261488af0
Beshoy Farag <beshoynady401@gmail.com>
2025-04-07 20:37:30 +0200

    finsih java hashset hackerrank

--- JavaHashSet.java
+++ JavaHashSet.java
@@ -1,33 +1,53 @@
 /*
-* we need to take the user input and split it into its componenets
-* we then take another line from the user and we split it
+* 5
+john tom
+john mary
+john tom
+mary anna
+mary anna
 *
+*1
+2
+2
+3
+3
 * */
 
 
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.ArrayList;
 
 
 public class JavaHashSet {
     public static void main(String [] args){
+
+
         Scanner scanner = new Scanner(System.in);
-        Set<String> userList = new HashSet<String>();
-        int queries = 0;
-        queries = scanner.nextInt(); scanner.nextLine();
-        int [] uniqueness = new int[queries];
+        Set<ArrayList<String>> sets = new HashSet<>();
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        String input;
+        String firstName;
+        String lastName;
+        int namesNumber = scanner.nextInt();scanner.nextLine();
 
 
-        for(int i = 0; i < queries; i++){
-            userList.add(scanner.nextLine().split(" ")[1]);
-            uniqueness[i] =  userList.size();
+        for(int i = 0; i < namesNumber; i++){
+            input = scanner.nextLine();
+            firstName = input.split(" ")[0];
+            lastName = input.split(" ")[1];
+            ArrayList<String> names = new ArrayList<>();
+            names.add(firstName);
+            names.add(lastName);
+            sets.add(names);
+            nums.add(sets.size());
 
         }
-        for (int i = 0; i < queries; i++){
-            System.out.println(uniqueness[i]);
+        for (int n : nums){
+            System.out.println(n);
         }
-
 
 
 
